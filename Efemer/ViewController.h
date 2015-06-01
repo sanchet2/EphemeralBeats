79f1c4d3410d64017e1e3f785ce23683d6d42780
Nikhil Sancheti <nsancheti1993@gmail.com>
2015-05-31 21:18:39 -0700

    Initial Commit

---
+++ Efemer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Efemer
+//
+//  Created by Nikhil Sancheti on 5/31/15.
+//  Copyright (c) 2015 Nikhil Sancheti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Efemer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Efemer
+//
+//  Created by Nikhil Sancheti on 5/31/15.
+//  Copyright (c) 2015 Nikhil Sancheti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
